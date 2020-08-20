79a2b360521f5973fb1ae7b81d84fbe0a2e1adaf
Josh Montoya <josh@montoya.io>
2020-08-20 10:59:03 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Hatch1fy/httpserve"
+)
+
+const (
+	defaultMethods        = "POST, GET, OPTIONS, PUT, DELETE"
+	defaultAllowedHeaders = "Accept, Content-Type, Content-Length, Accept-Encoding, Origin, X-UTC-OFFSET"
+)
+
+// Init is called when Vroomy initializes the plugin
+func Init(env map[string]string) (err error) {
+	return
+}
+
+// CORs will enable CORs
+func CORs(args ...string) (h httpserve.Handler, err error) {
+	var (
+		url            string
+		methods        string
+		allowedHeaders string
+	)
+
+	switch len(args) {
+	case 1:
+		url = args[0]
+		methods = defaultMethods
+		allowedHeaders = defaultAllowedHeaders
+	case 2:
+		url = args[0]
+		methods = args[1]
+		allowedHeaders = defaultAllowedHeaders
+	case 3:
+		url = args[0]
+		methods = args[1]
+		allowedHeaders = args[2]
+
+	default:
+		err = fmt.Errorf("invalid number of arguments, expected 1, 2, or 3 arguments and recieved %d", len(args))
+		return
+	}
+
+	h = newHandler(url, methods, allowedHeaders)
+	return
+}
+
+func newHandler(url, methods, allowedHeaders string) httpserve.Handler {
+	return func(ctx *httpserve.Context) (res httpserve.Response) {
+		hdr := ctx.Writer.Header()
+		hdr.Set("Access-Control-Allow-Origin", url)
+		hdr.Set("Access-Control-Allow-Methods", methods)
+		hdr.Set("Access-Control-Allow-Headers", allowedHeaders)
+		return
+	}
+}
